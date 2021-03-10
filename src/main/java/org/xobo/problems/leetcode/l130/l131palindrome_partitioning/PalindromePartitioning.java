bacc7537f8e837770e1a735036f487866593b8fc
Bing <Bing@xobo.org>
2021-03-10 14:26:10 +0800

    init

---
+++ src/main/java/org/xobo/problems/leetcode/l130/l131palindrome_partitioning/PalindromePartitioning.java
@@ -0,0 +1,7 @@
+package org.xobo.problems.leetcode.l130.l131palindrome_partitioning;
+
+import java.util.List;
+
+public interface PalindromePartitioning {
+  List<List<String>> partition(String s);
+}
